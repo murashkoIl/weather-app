59673fd4a07254b2303f14ce8712bc45b1e05be6
murashkoIl <muravey.ilya@mail.ru>
2022-06-03 23:06:36 +0300

    added home page template file

--- src/js/home/home.js
+++ src/js/home/home.js
@@ -1,8 +1,8 @@
 import { getCurrentWeather, BASE_URL } from '../weather';
 import { renderCustomNotification } from '../error/error';
+import { homePageTemplate } from '../../templates/home.template';
 
 export const renderHomePage = data => {
-	const dayIndex = new Date(data.forecast.forecastday[0].date).getDay();
 	const getDayName = dayIndex => {
 		const days = [
 			'Sunday',
@@ -18,146 +18,8 @@ export const renderHomePage = data => {
 
 	const storage = JSON.parse(window.localStorage.getItem('storage'));
 
-	return `
-    <header>
-    <div class="container">
-        <div class="header-wrapper">
-            <div class="header-info">
-                <div class="header-info-wrapper">
-                  <div class="header-return"><i class="fa-solid fa-arrow-left-long"></i></div>
-                  <div class="header-city">${data.location.name}</div>
-                </div>
-                <div class="header-temparature">${
-									storage.isCelcius ? data.current.temp_c : data.current.temp_f
-								}º</div>
-                <div class="header-status">${data.current.condition.text}</div>
-            </div>
-            <div class="header-icon"><img src="${
-							data.current.condition.icon
-						}" alt="Weather Icon"></div>
-        </div>
-        <div class="weather__info-container">
+	return homePageTemplate(data, storage, getDayName);
 
-            <div class="weather__info-humidity-wrapper">
-                <div class="weather__info-humidity-icon"><i class="fa-solid fa-droplet"></i></div>
-                <div class="weather__info-humidity">${
-									data.current.humidity
-								}%</div>
-            </div>
-
-            
-            <div class="weather__info-pressure-wrapper">
-                <div class="weather__info-pressure-icon"> <i class="fa-solid fa-circle-exclamation"></i></div>
-                <div class="weather__info-pressure">${
-									data.current.pressure_in
-								}mBar</div>
-            </div>
-            <div class="weather__info-wind-wrapper">
-                <div class="weather__info-wind-icon"><i class="fa-solid fa-wind"></i></div>
-                <div class="weather__info-wind">${
-									storage.isKPH
-										? data.current.wind_kph + 'km/h'
-										: data.current.wind_mph + 'm/s'
-								}</div>
-            </div>
-            
-
-        </div>
-    </div>
-  </header>
-  <section class="sunset-sunrise">
-    <div class="container">
-        <div class="sunset-wrapper">
-            <div class="sunset-icon"><i class="fa-solid fa-moon"></i></div>
-            <div class="sunset">${
-							data.forecast.forecastday[0].astro.sunset
-						}</div>
-        </div>
-        <div class="sunset-sunrise-divider"></div>
-        <div class="sunrise-wrapper">
-            <div class="sunrise-icon"><i class="fa-solid fa-sun"></i></div>
-            <div class="sunrise">${
-							data.forecast.forecastday[1].astro.sunrise
-						}</div>
-        </div>
-    </div>
-  </section>
-  <section class="today__temp">
-    <div class="container">
-        <div class="today__temp-container">
-            <div class="today__temp-flex-container">
-                ${data.forecast.forecastday[0].hour
-									.map(hour => {
-										return `
-                    <div class="today__temp-item today-item">
-                      <div class="today-item__time">${hour.time.substr(
-												10,
-												hour.time.length
-											)}</div>
-                      <div class="today-item__icon"><img src="${
-												hour.condition.icon
-											}" alt="Wether Icon"></div>
-                      <div class="today-item__degree">${
-												storage.isCelcius ? hour.temp_c : hour.temp_f
-											}º</div>
-                    </div>
-                  `;
-									})
-									.join('')}
-        </div>
-    </div>
-  </section>
-  <section class="week__temp">
-    <div class="container">
-        <div class="week__temp-container">
-            <div class="week__temp-item week-item">
-                <div class="week-item__day item">${getDayName(
-									dayIndex + 1
-								)}</div>
-                <div class="week-item__day item">${getDayName(
-									dayIndex + 2
-								)}</div>
-            </div>
-
-            <div class="week__temp-item week-item">
-                <div class="week-item__icon item"><img src="${
-									data.forecast.forecastday[1].day.condition.icon
-								}" alt="Weather Icon"></div>
-                <div class="week-item__icon item"><img src="${
-									data.forecast.forecastday[2].day.condition.icon
-								}" alt="Weather Icon"></div>
-            </div>
-
-            <div class="week__temp-item week-item">
-                <div class="week-item__daytemp item">${
-									storage.isCelcius
-										? data.forecast.forecastday[1].day.maxtemp_c
-										: data.forecast.forecastday[1].day.maxtemp_f
-								}º</div>
-                <div class="week-item__daytemp item">${
-									storage.isCelcius
-										? data.forecast.forecastday[2].day.maxtemp_c
-										: data.forecast.forecastday[2].day.maxtemp_f
-								}º</div>
-            </div>
-
-            <div class="week__temp-item week-item">
-                <div class="week-item__nighttemp item">${
-									storage.isCelcius
-										? data.forecast.forecastday[1].day.mintemp_c
-										: data.forecast.forecastday[2].day.mintemp_f
-								}º</div>
-                <div class="week-item__nighttemp item">${
-									storage.isCelcius
-										? data.forecast.forecastday[2].day.mintemp_c
-										: data.forecast.forecastday[2].day.mintemp_f
-								}º</div>
-            </div>
-        </div>
-    </div>
-  </section>
-
-  `;
 };
 
 export const constructHomePage = () => {
--- src/js/saved/saved.js
+++ src/js/saved/saved.js
@@ -1,122 +1,12 @@
 import { getCurrentWeather, BASE_URL, getCities } from './../weather';
 import { renderCustomNotification } from '../error/error';
-
-export const renderSavedPage = () => {
-	return `
-    <header class="header">
-      <div class="container">
-          <form class="search-form" type="submit" action="#">
-              <div class="input-wrapper">
-                  <a class="input-search-icon"><i class="fa-solid fa-magnifying-glass"></i></a>
-                  <input class="input-search" type="text" placeholder="Search">
-                  
-              </div>
-          </form>
-      </div>
-    </header>
-    <section class="saved-cities">
-      <div class="container">s
-          <div class="saved-cities-wrapper">
-            ${renderSavedCities()}
-          </div>
-      </div>
-    </section>
-  `;
-};
-
-export const renderSavedCities = () => {
-	const storage = updateCurrentWeather();
-	return `
-    ${storage.cards
-			.map(card => {
-				return `
-                <div data-id="${card.city}" class="saved-cities-item city">
-                  <div data-id="${card.city}" class="city-delete">x</div>
-                  <div class="city-deg-icon">
-                      <div class="city-deg">${
-												storage.isCelcius ? card.temp_c : card.temp_f
-											}º</div>
-                      <div class="city-icon"><img src="${card.icon}"></div>
-                  </div>
-                  <div class="city-name">${card.city}</div>
-                  <div class="country-name">${card.country}</div>
-                  <div class="city-weather-info">
-                      
-                      <div class="city-weather-humidity-wrapper">
-                          <div class="city-weather-humidity-icon"><i class="fa-solid fa-droplet"></i></div>
-                          <div class="city-weather-humidity">${
-														card.humidity
-													}%</div>
-                      </div>
-                      <div class="city-weather-wind-wrapper">
-                          <div class="city-weather-wind-icon"><i class="fa-solid fa-wind"></i></div>
-                          <div class="city-weather-wind">${
-														storage.isKPH
-															? card.wind_kph + 'km/h'
-															: card.wind_mph + 'm/s'
-													}</div>
-                      </div>
-                      
-                  </div>
-                </div>
-              `;
-			})
-			.join('')}
-  `;
-};
-
-export const updateCurrentWeather = () => {
-	const storage = JSON.parse(window.localStorage.getItem('storage'));
-	storage.cards.forEach((card, index) => {
-		getCurrentWeather(BASE_URL, card.city)
-			.then(info => {
-				const updatedCard = {
-					city: info.location.name,
-					country: info.location.country,
-					temp_c: info.current.temp_c,
-					temp_f: info.current.temp_f,
-					icon: info.current.condition.icon,
-					humidity: info.current.humidity,
-					wind_kph: info.current.wind_kph,
-					wind_mph: info.current.wind_mph,
-				};
-				storage.cards[index] = updatedCard;
-				window.localStorage.setItem('storage', JSON.stringify(storage));
-			})
-			.catch(err => renderCustomNotification(err));
-	});
-	return storage;
-};
-
-export const renderFoundCities = data => {
-	return `
-  <div class="found-cities">
-    ${data
-			.map(city => {
-				return `
-        <div data-name="${city.name}" class="found-cities-item city-found">
-          <div class="city-found-name">${city.name}</div>
-          <div class="city-found-icon"><i class="fa-solid fa-heart search"></i></div>
-        </div>
-      `;
-			})
-			.join('')}
-  </div>
-  `;
-};
-
-export const saveCity = card => {
-	const storage = JSON.parse(window.localStorage.getItem('storage'));
-	storage.cards.push(card);
-	window.localStorage.setItem('storage', JSON.stringify(storage));
-};
-
-export const deleteCity = city => {
-	const storage = JSON.parse(localStorage.getItem('storage'));
-	const filtered = storage.cards.filter(card => card.city !== city);
-	storage.cards = [...filtered];
-	localStorage.setItem('storage', JSON.stringify(storage));
-};
+import {
+	renderFoundCities,
+	deleteCity,
+	saveCity,
+	isCitiesRendered,
+	renderSavedPage
+} from './savedHelpers';
 
 export const toFocusInput = () => {
 	document.querySelector('.input-search').focus();
@@ -169,13 +59,6 @@ export const constructSavedPage = () => {
 	};
 };
 
-export const isCitiesRendered = () => {
-	const citiesWrapper = document.querySelector('.found-cities-wrapper');
-	if (citiesWrapper) {
-		citiesWrapper.remove();
-	}
-};
-
 export const handleWrapperListener = event => {
 	if (
 		event.target.closest('.city') &&
@@ -192,41 +75,43 @@ export const handleWrapperListener = event => {
 		constructSavedPage();
 	}
 	if (event.target.closest('.city-found')) {
-		const targetCityName = event.target.closest('.city-found').dataset.name;
-		let isCitiesEquals = false;
-
-		JSON.parse(localStorage.getItem('storage')).cards.forEach(card => {
-			if (card.city.includes(targetCityName)) {
-				renderCustomNotification('This city already in the list!');
-				isCitiesEquals = true;
-			}
-		});
-
-		if (isCitiesEquals) return;
-
-		getCurrentWeather(
-			BASE_URL,
-			event.target.closest('.city-found').dataset.name
-		)
-			.then(data => {
-				const card = {
-					city: data.location.name,
-					country: data.location.country,
-					temp_c: data.current.temp_c,
-					temp_f: data.current.temp_f,
-					icon: data.current.condition.icon,
-					humidity: data.current.humidity,
-					wind_kph: data.current.wind_kph,
-					wind_mph: data.current.wind_mph,
-				};
-				saveCity(card);
-			})
-			.then(() => {
-				constructSavedPage();
-			})
-			.catch(err => renderCustomNotification(err));
+		foundCityClickingHandler(event);
 	}
 	if (event.target.closest('.input-search-icon')) {
 		toFocusInput();
 	}
 };
+
+export const foundCityClickingHandler = event => {
+	const targetCityName = event.target.closest('.city-found').dataset.name;
+	let isCitiesEquals = false;
+
+	JSON.parse(localStorage.getItem('storage')).cards.forEach(card => {
+		if (card.city.includes(targetCityName)) {
+			renderCustomNotification('This city already in the list!');
+			isCitiesEquals = true;
+		}
+	});
+
+	if (isCitiesEquals) return;
+
+	getCurrentWeather(BASE_URL, event.target.closest('.city-found').dataset.name)
+		.then(data => {
+			const card = {
+				city: data.location.name,
+				country: data.location.country,
+				temp_c: data.current.temp_c,
+				temp_f: data.current.temp_f,
+				icon: data.current.condition.icon,
+				humidity: data.current.humidity,
+				wind_kph: data.current.wind_kph,
+				wind_mph: data.current.wind_mph,
+			};
+			saveCity(card);
+		})
+		.then(() => {
+			constructSavedPage();
+		})
+		.catch(err => renderCustomNotification(err));
+};
+
